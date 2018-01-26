41602dba084a4104a41c9028d267044952b0ffc7
Andrew Watkins <watts@influxdb.com>
2018-01-26 10:30:18 -0700

    Sterf

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
-app.set('port', process.env.PORT || 3000);
-app.locals.title = 'Test Express';
+app.set('port', process.env.PORT || 3000)
+app.locals.title = 'Test Express'
+app.locals.students = require('./students.js')
 
 app.get('/', (request, response) => {
-  response.send('We\'re going to test all the routes!');
-});
+  response.send("We're going to test all the routes!")
+})
 
-app.listen(app.get('port'), () => {
-  console.log(`${app.locals.title} is running on localhost:${app.get('port')}.`);
-});
+app.get('/api/v1/students', (request, response) => {
+  return response.status(200).json(app.locals.students)
+})
 
-module.exports = app;
+module.exports = app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on localhost:${app.get('port')}.`)
+})
---
+++ test/routes.spec.js
@@ -0,0 +1,45 @@
+const chai = require('chai')
+const should = chai.should()
+const chaiHttp = require('chai-http')
+const server = require('../server')
+const pry = require('pryjs')
+
+chai.use(chaiHttp)
+
+describe('All the routes', () => {
+  after(done => {
+    server.close(done)
+  })
+
+  describe('Client Routes', () => {
+    it('should return the homepage with text', async () => {
+      const response = await chai.request(server).get('/')
+
+      response.should.have.status(200)
+      response.should.be.html
+      response.res.text.should.equal("We're going to test all the routes!")
+    })
+
+    it('should return a 404 for a route that does not exist', async () => {
+      const response = await chai.request(server).get('/sad')
+      response.should.have.status(404)
+    })
+  })
+
+  describe('API Routes', () => {
+    describe('GET /api/v1/students', () => {
+      it('should return all of the students', async () => {
+        try {
+          const response = await chai.request(server).get('/api/v1/students')
+          response.should.have.status(200)
+          response.should.be.json
+          response.body.should.be.a('array')
+          response.body.length
+        } catch (error) {
+          console.error(error)
+          throw error
+        }
+      })
+    })
+  })
+})
