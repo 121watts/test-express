3791fae227d06872340f1679038c95aa437cb012
Andrew Watkins <watts@influxdb.com>
2018-01-31 16:28:32 -0700

    Add postgres step to circle

--- knexfile.js
+++ knexfile.js
@@ -18,4 +18,15 @@ module.exports = {
     },
     useNullAsDefault: true,
   },
+  test: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL || 'postgres://localhost/publications',
+    useNullAsDefault: true,
+    migrations: {
+      directory: './db/migrations',
+    },
+    seeds: {
+      directory: './db/test/seeds',
+    },
+  },
 }
