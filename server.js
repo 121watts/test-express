57a18299c947134ea87529a7b368499a7bd71638
Andrew Watkins <watts@influxdb.com>
2018-02-01 07:22:51 -0700

    Stupid prettier

--- server.js
+++ server.js
@@ -1,28 +1,30 @@
-const environment = process.env.NODE_ENV || 'development'
-const configuration = require('./knexfile')[environment]
-const database = require('knex')(configuration)
+const environment = process.env.NODE_ENV || "development";
+const configuration = require("./knexfile")[environment];
+const database = require("knex")(configuration);
 
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.set('port', process.env.PORT || 3000)
-app.locals.title = 'Test Express'
+app.set("port", process.env.PORT || 3000);
+app.locals.title = "Test Express";
 
-app.get('/', (request, response) => {
-  response.send("I am not a continuous integrator")
-})
+app.get("/", (request, response) => {
+  response.send("We're going to test all the routes!");
+});
 
-app.get('/api/v1/papers', (request, response) => {
-  database('papers')
+app.get("/api/v1/papers", (request, response) => {
+  database("papers")
     .select()
     .then(papers => {
-      response.status(200).json(papers)
+      response.status(200).json(papers);
     })
     .catch(error => {
-      response.status(500).json({error})
-    })
-})
+      response.status(500).json({ error });
+    });
+});
 
-module.exports = app.listen(app.get('port'), () => {
-  console.log(`${app.locals.title} is running on localhost:${app.get('port')}.`)
-})
+module.exports = app.listen(app.get("port"), () => {
+  console.log(
+    `${app.locals.title} is running on localhost:${app.get("port")}.`
+  );
+});
--- test/routes.spec.js
+++ test/routes.spec.js
@@ -1,40 +1,39 @@
-const chai = require('chai')
-const should = chai.should()
-const chaiHttp = require('chai-http')
-const server = require('../server')
+const chai = require("chai");
+const should = chai.should();
+const chaiHttp = require("chai-http");
+const server = require("../server");
 
-chai.use(chaiHttp)
+chai.use(chaiHttp);
 
-describe('All the routes', () => {
-  describe('Client Routes', () => {
-    it('should return the homepage with text', async () => {
-      const response = await chai.request(server).get('/')
+describe("All the routes", () => {
+  describe("Client Routes", () => {
+    it("should return the homepage with text", async () => {
+      const response = await chai.request(server).get("/");
 
-      response.should.have.status(200)
-      response.should.be.html
-      response.res.text.should.equal("We're going to test all the routes!")
-    })
+      response.should.have.status(200);
+      response.should.be.html;
+    });
 
-    it('should return a 404 for a route that does not exist', async () => {
-      const response = await chai.request(server).get('/sad')
-      response.should.have.status(404)
-    })
-  })
+    it("should return a 404 for a route that does not exist", async () => {
+      const response = await chai.request(server).get("/sad");
+      response.should.have.status(404);
+    });
+  });
 
-  describe('API Routes', () => {
-    describe('GET /api/v1/papers', () => {
-      it('should return all of the papers', async () => {
+  describe("API Routes", () => {
+    describe("GET /api/v1/papers", () => {
+      it("should return all of the papers", async () => {
         try {
-          const response = await chai.request(server).get('/api/v1/papers')
-          response.should.have.status(200)
-          response.should.be.json
-          response.body.should.be.a('array')
-          response.body.length
+          const response = await chai.request(server).get("/api/v1/papers");
+          response.should.have.status(200);
+          response.should.be.json;
+          response.body.should.be.a("array");
+          response.body.length;
         } catch (error) {
-          console.error(error)
-          throw error
+          console.error(error);
+          throw error;
         }
-      })
-    })
-  })
-})
+      });
+    });
+  });
+});
