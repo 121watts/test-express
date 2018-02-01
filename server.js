57bf67a1b5f70ff768d8e114979ebec09a1f1401
Andrew Watkins <watts@influxdb.com>
2018-01-31 21:04:39 -0700

    Loggin the env

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const environment = process.env.NODE_ENV || 'development'
+console.log('HERES your process being set', process.env)
 const configuration = require('./knexfile')[environment]
 const database = require('knex')(configuration)
 
