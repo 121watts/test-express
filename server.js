c95457d66ccaf442c5cec70051cd218a1361bc0b
Andrew Watkins <watts@influxdb.com>
2018-02-01 07:32:54 -0700

    Change copy

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.set('port', process.env.PORT || 3000)
 app.locals.title = 'Test Express'
 
 app.get('/', (request, response) => {
-  response.send("We're going to test all the routes!")
+  response.send('We are not continuous integrators')
 })
 
 app.get('/api/v1/papers', (request, response) => {
